e31b2bb5a8bae4446110437bbf06425e38933ffe
Matheus Severo <matheus.severo02@gmail.com>
2025-04-15 10:25:54 -0300

    Reports

---
+++ src/FinanceSystem.Web/Views/Reports/CreditCards.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceSystem.Web.Views.Reports
+{
+    public class CreditCardsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
