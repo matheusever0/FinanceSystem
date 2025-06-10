d6cd604c4266f3b3b2cbbea0e0c38f600f34109b
Matheus Severo <matheus.severo02@gmail.com>
2025-06-09 23:19:51 -0300

    Ajuste views

---
+++ src/Equilibrium.Web/Models/Commons/ActionButtonsModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Equilibrium.Web.Models.Commons
+{
+    public class ActionButtonsModel
+    {
+        public string Id { get; set; }
+        public bool CanView { get; set; }
+        public bool CanEdit { get; set; }
+        public bool CanDelete { get; set; }
+        public string ControllerName { get; set; }
+        public DeleteButtonModel DeleteConfig { get; set; }
+        public string ExtraButtons { get; set; }
+
+        public ActionButtonsModel()
+        {
+        }
+
+        public ActionButtonsModel(string id, bool canView, bool canEdit, bool canDelete,
+            string controllerName, DeleteButtonModel deleteConfig = null, string extraButtons = "")
+        {
+            Id = id;
+            CanView = canView;
+            CanEdit = canEdit;
+            CanDelete = canDelete;
+            ControllerName = controllerName;
+            DeleteConfig = deleteConfig;
+            ExtraButtons = extraButtons ?? "";
+        }
+    }
+}
